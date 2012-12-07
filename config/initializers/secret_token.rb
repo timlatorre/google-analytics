670c06d5aec0e718085c2b8b16fea999c9214571
Tim LaTorre <timlatorre@gmail.com>
2012-12-07 14:45:48 -0500

    heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleAnalytics::Application.config.secret_token = '1f5ae021238f05387a2461d496f48f5cda016799fcf225fc74fbc4b3b036e63b44e0792df59da64a3ee49dad6bb855bf370b7f0ebd0c5749769e03ee69e3b2a9'
